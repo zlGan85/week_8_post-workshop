259bdb7169a6fb49cb91e3379af1f9351bee98ed
zlgan85 <zgan0022@student.monash.edu>
2026-05-01 17:55:26 +0800

    mod V1

--- 34598499.c
+++ 34598499.c
@@ -4,10 +4,10 @@
 *******************************************************************************************************
 The main() function of the following C program calls a user-defined function filter_ascending()
 utilizing the following input arguments:
-1. A pointer to the first node of a linked list containing nodes of type struct node 
+1. A pointer to the first node of a linked list containing nodes of type struct node
 2. The count of nodes in the linked list
 
-The function filters the nodes within the linked list in ascending order by deleting the nodes that 
+The function filters the nodes within the linked list in ascending order by deleting the nodes that
 do not meet this requirement.
 
 For example:
@@ -18,16 +18,16 @@ If the original linked list passed into the filter_ascending() function is:
 The modified linked list after executing the function should be as follows:
 10->17->25->35->NULL
 
-1. The definition of the function filter_ascending() is however missing two lines of code. You must 
+1. The definition of the function filter_ascending() is however missing two lines of code. You must
 write these two missing lines of code within the function definition and verify that the program
-provides the correct output utilizing your own test cases. 
+provides the correct output utilizing your own test cases.
 
-2. In the process of correcting the program, you are allowed to only add the missing two lines of code 
-within the definition of the filter_ascending() function. In case the program is subjected to any 
+2. In the process of correcting the program, you are allowed to only add the missing two lines of code
+within the definition of the filter_ascending() function. In case the program is subjected to any
 other changes, no marks will be awarded.
 
-2. You are required to submit your solution as a single .c file with the name <studentID>.c such that 
-<studentID> is replaced with your unique student ID, for example 12345678.c. Note that a penalty will 
+2. You are required to submit your solution as a single .c file with the name <studentID>.c such that
+<studentID> is replaced with your unique student ID, for example 12345678.c. Note that a penalty will
 apply for not following these instructions or the naming conventions.
 *******************************************************************************************************
 */
@@ -96,6 +96,8 @@ void filter_ascending(struct node *startPtr, int count_nodes)
 {
     struct node* prevPtr = NULL;
     struct node* curPtr = NULL;
+	if (startPtr == NULL) return; // Added: safety check
+
 	prevPtr = startPtr;
 	curPtr = startPtr->nextPtr;
     for (int i = 0; i < count_nodes; i++)
@@ -106,8 +108,9 @@ void filter_ascending(struct node *startPtr, int count_nodes)
             {
                 prevPtr->nextPtr = curPtr->nextPtr;
                 free(curPtr);
+				curPtr = prevPtr->nextPtr; // Added: assign curPtr back to a node instead of point to empty node
 			}
-            prevPtr = prevPtr->nextPtr; 
+            prevPtr = prevPtr->nextPtr;
             curPtr = prevPtr->nextPtr;
         }
     }
